956ebfaf73a45bba8f554594a0c748eddd802f3e
Piero Paialunga <piero.paialunga@hotmail.com>
2025-04-16 09:43:46 -0400

    First commit

---
+++ utils.py
@@ -0,0 +1,9 @@
+import numpy as np
+from scipy.stats import norm
+
+def black_scholes_solution(S, K, T, r, sigma):
+    S = np.array(S)
+    d1 = (np.log(S / K) + (r + 0.5 * sigma**2) * T) / (sigma * np.sqrt(T))
+    d2 = d1 - sigma * np.sqrt(T)
+    return S * norm.cdf(d1) - K * np.exp(-r * T) * norm.cdf(d2)
+
